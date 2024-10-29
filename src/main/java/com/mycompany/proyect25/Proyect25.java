0bed39fd3bb85ece09d9d92b5a2aa29fe4d61167
Zanyn-user <zanynuser@gmail.com>
2024-10-29 10:39:26 -0500

    primer comentario

---
+++ src/main/java/com/mycompany/proyect25/Proyect25.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.proyect25;
+
+/**
+ *
+ * @author User
+ */
+public class Proyect25 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
